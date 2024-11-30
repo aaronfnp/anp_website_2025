d08ef6a820f3de5e4f6bca2e774f7b8599ab7835
aaronfnp <aaronfnp@gmail.com>
2024-11-30 13:51:18 -0500

    created fully functioning timer

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -3,6 +3,14 @@ import React, { useState, useEffect } from "react";
 export default function Timer() {
   const [time, setTime] = useState(0);
 
+  const bodyText = {
+    days: "I think this was just left on in the background...",
+    hours: "At this point I hope you've already hired me",
+    minutes:
+      "Thanks for giving my portfolio a thorough lookthrough! Please consider contacting me about working together!",
+    seconds: "Check out my projects above and let me know what you think!",
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
       setTime((prev) => prev + 1);
@@ -17,9 +25,22 @@ export default function Timer() {
   const seconds = time % 60;
 
   return (
-    <h1>
-      You have been looking at this page for: {days} days, {hours} hours,{" "}
-      {minutes} minutes and {seconds} seconds
-    </h1>
+    <div>
+      <h3>
+        You have been looking my portfolio for:{" "}
+        {days >= 1 ? `${days} days,` : ""} {hours >= 1 ? `${hours} hours,` : ""}{" "}
+        {minutes >= 1 ? `${minutes} minutes, ` : ""}
+        {seconds} seconds
+      </h3>
+      <h3>
+        {days >= 1
+          ? bodyText.days
+          : hours >= 1
+          ? bodyText.hours
+          : minutes >= 5
+          ? bodyText.minutes
+          : bodyText.seconds}
+      </h3>
+    </div>
   );
 }
