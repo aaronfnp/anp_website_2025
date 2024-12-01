61ab99a682f9ecb30852b1fd94eb8c47fa4a7ab1
aaronfnp <aaronfnp@gmail.com>
2024-11-30 23:53:02 -0500

    Added Sticky Header
    - Added sticky class to header to make sure it stays at the top after scrolling past
    - Added z-10 to ensure it doesn't go away when you hit the bottom

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-black">
+    <header className="bg-black sticky top-0 z-10">
       <nav
         aria-label="Global"
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
