33a21eb82f9dfeda47a6ca93b2fc600a4fe59a63
aaronfnp <aaronfnp@gmail.com>
2024-11-30 20:26:27 -0500

    Added project info as props
    - Added all project info as props to allow for use as general component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import ProjectContainer from "./components/layout/project-container";
 
 function App() {
   return (
-    <div className="relative h-screen w-full bg-slate-950">
+    <div>
       <div>
-        <Header />
         <h1>What do you get when you cross a VFX artist and coding?</h1>
+        <Header />
         <h1>About</h1>
         <h2>
           Driven software engineer with a strong foundation in frontend and
@@ -21,30 +21,34 @@ function App() {
           responsive, user-centered applications.
         </h2>
         <h1>Projects</h1>
-        <h2>Talent-Trees</h2>
-        <h3>
-          A dynamic web application for creating and sharing custom talent
-          trees, designed for gamers to personalize skill paths.
-        </h3>
-        <h4>Technologies: React, Firebase, Vite, Tailwind</h4>
-        <h3 href="">GitHub</h3> <h3 href="">Application</h3>
-        <h2>Colonel Kernel</h2>
-        <h3>
-          A clicker game where users earn &quot;corn&quot; to purchase upgrades,
-          within a team of four.
-        </h3>
-        <h4>Technologies: MongoDB, Express, React, Node.js</h4>
-        <h3 href="">GitHub</h3> <h3 href="">Application</h3>
-        <h2>Get Back To Work</h2>
-        <h3>
-          A 3D spot-the-difference game set in an office environment built in
-          Unity
-        </h3>
-        <h4>Technologies: Unity, C#, Blender</h4>
-        <h3 href="">GitHub</h3> <h3 href="">Application</h3>
-        <h1>Test APP</h1>
-        <ProjectContainer></ProjectContainer>
-        <Contact></Contact>
+        <ProjectContainer
+          title={"Talent-Trees"}
+          description={
+            "A dynamic web application for creating and sharing custom talent trees, designed for gamers to personalize skill paths."
+          }
+          tech={"React, Firebase, Vite, Tailwind"}
+          linkGit={""}
+          linkApp={""}
+        ></ProjectContainer>
+        <ProjectContainer
+          title={"Colonel Kernel"}
+          description={
+            'A clicker game where users earn "corn" to purchase upgrades, within a team of four.'
+          }
+          tech={"MongoDB, Express, React, Node.js"}
+          linkGit={""}
+          linkApp={""}
+        ></ProjectContainer>
+        <ProjectContainer
+          title={"Get Back To Work"}
+          description={
+            "A 3D spot-the-difference game set in an office environment built in Unity"
+          }
+          tech={"Unity, C#, Blender"}
+          linkGit={""}
+          linkApp={""}
+        ></ProjectContainer>
+        <Contact />
       </div>
     </div>
   );
