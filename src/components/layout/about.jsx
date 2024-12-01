b320448e80e83b7f82738a873310618758267c2b
aaronfnp <aaronfnp@gmail.com>
2024-11-30 21:56:35 -0500

    added icons and columns
    - Added icons for technologies used
    - Added some basic styling to break up into columns

--- src/components/layout/about.jsx
+++ src/components/layout/about.jsx
@@ -4,15 +4,46 @@ function About() {
   return (
     <div className="about-section p-8">
       <h1>About</h1>
-      <h2>
-        Driven software engineer with a strong foundation in frontend and
-        full-stack web development, transitioning from a successful background
-        in VFX editing. My experience in VFX and game development (Unity & C#)
-        has honed my attention to detail and creativity, while General
-        Assembly&apos;s Software Engineering program solidified my technical
-        skills, especially in JavaScript and React. Passionate about creating
-        responsive, user-centered applications.
-      </h2>
+      <div className="flex columns-2">
+        <div>
+          <h2>
+            Driven software engineer with a strong foundation in frontend and
+            full-stack web development, transitioning from a successful
+            background in VFX editing. My experience in VFX and game development
+            (Unity & C#) has honed my attention to detail and creativity, while
+            General Assembly&apos;s Software Engineering program solidified my
+            technical skills, especially in JavaScript and React. Passionate
+            about creating responsive, user-centered applications.
+          </h2>
+        </div>
+        <div>
+          <div className="flex-col columns-5">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/unity/unity-original.svg" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
