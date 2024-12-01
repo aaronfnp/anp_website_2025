ee9795d886076fe7875ec697dffd6a6c700550c8
aaronfnp <aaronfnp@gmail.com>
2024-12-01 10:28:47 -0500

    contact form styling
    - Made contact form have name and email next to each other except on small screens
    - Removed some margins to condense the form
    - Made confirm button be on the right

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
   const [isSubmittingDialog, setIsSubmittingDialog] = useState(false);
 
   return (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="isolate bg-white px-6 py-20 sm:py-32 lg:px-8">
       <DialogWindow
         isSubmittingDialog={isSubmittingDialog}
         setIsSubmittingDialog={setIsSubmittingDialog}
@@ -21,51 +21,49 @@ export default function Contact() {
         <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
           Contact Me
         </h2>
-        <p className="mt-2 text-lg/8 text-gray-600">
+        <p className="mt-5 text-lg/8 text-gray-600">
           <Timer />
         </p>
       </div>
-      <form
-        action="#"
-        method="POST"
-        className="mx-auto mt-16 max-w-xl sm:mt-20"
-      >
+      <form action="#" method="POST" className="mx-auto mt-8 max-w-xl sm:mt-12">
         <div className="sm:col-span-2">
-          <div>
-            <label
-              htmlFor="name"
-              className="block text-sm/6 font-semibold text-gray-900"
-            >
-              Name
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="name"
-                name="name"
-                type="text"
-                autoComplete="full-name"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
+          <div className="flex flex-col sm:flex-row sm:space-x-4">
+            <div className="flex-1">
+              <label
+                htmlFor="name"
+                className="block text-sm/6 font-semibold text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+            <div className="flex-1 mt-4 sm:mt-0">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-semibold text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
             </div>
           </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm/6 font-semibold text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-              />
-            </div>
-          </div>
-          <div className="sm:col-span-2">
+          <div className="sm:col-span-2 mt-4">
             <label
               htmlFor="message"
               className="block text-sm/6 font-semibold text-gray-900"
@@ -83,7 +81,7 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <div className="mt-10">
+        <div className="mt-8">
           <button
             type="button"
             onClick={(e) => {
