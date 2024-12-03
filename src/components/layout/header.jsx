39f8a8ae9a2cd9671af6e42f5458cfbd7babc7be
aaronfnp <aaronfnp@gmail.com>
2024-12-02 21:23:00 -0500

    Header styling
    - Added a drop shadow and changed container of the header to white
    - Changed font size, color, and capitalization
    - Added an underline border for when hover
    -- May change to when selected using states

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -8,14 +8,14 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-[#006645] sticky top-0 z-10 w-full">
+    <header className="sticky top-0 z-10 w-full bg-white shadow-lg">
       <nav
         aria-label="Global"
         className="mx-auto flex w-full items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+            <span className="sr-only">ANP</span>
             <img
               alt=""
               src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
@@ -33,18 +33,26 @@ export default function Header() {
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a href="#about" className="text-sm/6 font-semibold text-white-900">
-            About
-          </a>
+        <PopoverGroup className="hidden lg:flex lg:gap-x-12 text-[#006645]">
           <a
             href="#projects"
-            className="text-sm/6 font-semibold text-white-900"
+            className="text-lg font-semibold border-b-8 border-white hover:border-[#e7fdff] -mb-1 leading-tight"
           >
-            Projects
+            about
           </a>
-          <a href="#contact" className="text-sm/6 font-semibold text-white-900">
-            Contact
+          <div className="flex flex-col items-center">
+            <a
+              href="#projects"
+              className="text-lg font-semibold border-b-8 border-white hover:border-[#e7fdff] -mb-2 leading-tight"
+            >
+              projects
+            </a>
+          </div>
+          <a
+            href="#projects"
+            className="text-lg font-semibold border-b-8 border-white hover:border-[#e7fdff] -mb-1 leading-tight"
+          >
+            contact
           </a>
         </PopoverGroup>
       </nav>
@@ -57,7 +65,7 @@ export default function Header() {
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-ƒ-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">ANP</span>
               <img
                 alt=""
                 src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
@@ -75,24 +83,24 @@ export default function Header() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-white-500/10">
-              <div className="space-y-2 py-6">
+              <div className="space-y-2 py-6 text-[#006645]">
                 <a
                   href="#about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50"
                 >
-                  About
+                  about
                 </a>
                 <a
                   href="#projects"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50"
                 >
-                  Projects
+                  projects
                 </a>
                 <a
                   href="#contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50"
                 >
-                  Contact
+                  contact
                 </a>
               </div>
             </div>
