e86847ba056a1c2e1844251a632d58a32c40efba
aaronfnp <aaronfnp@gmail.com>
2024-12-02 23:53:55 -0500

    project container styling
    - Changed props from being individually destructured to the entire object to move the map function into the container.
    - Changed from reverse flex box set up to a grid set up.
    - Changed from row based to columns project containers
    - Added styling to texts (text-left, colors, sizes, bold, etc)

--- src/components/layout/project-container.jsx
+++ src/components/layout/project-container.jsx
@@ -1,58 +1,62 @@
-import React from "react";
-
-export default function ProjectContainer({
-  title,
-  description,
-  tech,
-  linkGit,
-  linkApp,
-  linkImg,
-  reverse,
-}) {
+const ProjectContainer = ({ projects }) => {
   return (
-    <div
-      className={`flex items-center justify-center mb-8 mt-8 ${
-        reverse ? "flex-row-reverse" : ""
-      }`}
-    >
-      <div className="mx-5 border-8 border-[#006645] rounded-lg hover:border-[#00A97C] drop-shadow-md">
-        <img
-          className="object-cover h-48 w-96 grayscale hover:grayscale-0"
-          src={linkImg}
-          alt={`${title} Thumbnail`}
-        />
-      </div>
-      <div className={`flex-col text-left ${reverse ? "text-right" : ""}`}>
-        <h2 className="text-2xl font-bold">{title}</h2>
-        <h3 className="text-md">{description}</h3>
-        <h4 className="text-sm font-bold">Technologies: {tech}</h4>
-        <div
-          className={`flex mt-5 ${
-            reverse ? "justify-end" : "justify-start"
-          } space-x-2`}
-        >
-          {linkGit && (
-            <a href={linkGit} className="flex items-center space-x-2">
+    <div>
+      <h1 className="ml-5 text-left font-bold">projects.</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mb-8 mt-8 justify-center place-items-start">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center mx-5 w-[350px] h-[450px] "
+          >
+            <div className="w-full h-[200px] flex items-center justify-center drop-shadow-md">
               <img
-                className="object-cover h-12 w-12"
-                src="https://img.icons8.com/?size=100&id=118557&format=png&color=000000"
-                alt="GitHub Icon"
+                className="object-cover h-full w-full"
+                src={project.linkImg}
+                alt={`${project.title} Thumbnail`}
               />
-              <span>GitHub</span>
-            </a>
-          )}
-          {linkApp && (
-            <a href={linkApp} className="flex items-center space-x-2">
-              <img
-                className="object-cover h-10 w-10"
-                src="https://img.icons8.com/?size=100&id=PdMS7nTd0vpH&format=png&color=000000"
-                alt="Application Icon"
-              />
-              <span>Application</span>
-            </a>
-          )}
-        </div>
+            </div>
+
+            <div className="flex-col text-left mt-4 flex-grow w-full">
+              <h2 className="text-2xl font-semibold text-slate-500">
+                {project.title}
+              </h2>
+              <p className="text-md text-slate-500">{project.description}</p>
+              <div className="mt-4 w-[30%] h-[4%] bg-[#e7fdff]"></div>
+              <h4 className="text-md my-1 font-bold text-[#00A97C]">
+                {project.tech}
+              </h4>
+              <div className="flex justify-start space-x-2">
+                {project.linkGit && (
+                  <a
+                    href={project.linkGit}
+                    className="flex items-center space-x-2"
+                  >
+                    <img
+                      className="object-cover h-8 w-8"
+                      src="https://img.icons8.com/?size=100&id=SzgQDfObXUbA&format=png&color=000000"
+                      alt="GitHub Icon"
+                    />
+                  </a>
+                )}
+                {project.linkApp && (
+                  <a
+                    href={project.linkApp}
+                    className="flex items-center space-x-2"
+                  >
+                    <img
+                      className="object-cover h-10 w-10"
+                      src="https://img.icons8.com/?size=100&id=PdMS7nTd0vpH&format=png&color=000000"
+                      alt="Application Icon"
+                    />
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default ProjectContainer;
