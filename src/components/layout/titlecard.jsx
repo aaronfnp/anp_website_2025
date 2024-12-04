f052fc06fcddaf6366c7046ec79d841348fb2d12
aaronfnp <aaronfnp@gmail.com>
2024-12-04 17:04:25 -0500

    added drop shadow to title
    - added dropshadow to titles

--- src/components/layout/titlecard.jsx
+++ src/components/layout/titlecard.jsx
@@ -22,13 +22,12 @@ const TitleCard = () => {
         <source src={ANPReel} type="video/mp4" />
       </video>
 
-      {/* Overlay Content */}
       <div className="relative z-10 flex flex-col justify-center items-center h-full text-[#f6f9ff]">
         <div
           className="relative group text-center cursor-pointer"
           onClick={() => setIsTitleClicked(true)}
         >
-          <h1 className="flex flex-col text-[#f6f9ff] my-4 text-[10rem] font-bebas font-extrabold drop-shadow-2xl ease-in-out transform transition-all whitespace-nowrap">
+          <h1 className="flex flex-col text-[#f6f9ff] my-4 text-[10rem] font-bebas font-extrabold drop-shadow-4xl ease-in-out transform transition-all whitespace-nowrap">
             {/* "WHAT DO YOU GET" SLIDES OUT LEFT, INNOVATIVE SLIDES IN */}
             <div className="inline-block">
               <h2 className="flex justify-between w-full">
@@ -37,7 +36,7 @@ const TitleCard = () => {
                     isTitleClicked
                       ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50"
                       : "translate-x-20 opacity-100"
-                  }`}
+                  } drop-shadow-4xl`}
                 >
                   WHAT DO YOU GET
                 </span>
@@ -46,7 +45,7 @@ const TitleCard = () => {
                     isTitleClicked
                       ? "-translate-x-20 opacity-100 "
                       : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50"
-                  }`}
+                  } drop-shadow-4xl`}
                 >
                   {titles.title1}
                 </span>
@@ -57,11 +56,11 @@ const TitleCard = () => {
             <div className="inline-block">
               <h2 className="flex justify-between w-full">
                 <span
-                  className={`duration-700 text-left  ${
+                  className={`duration-700 text-left ${
                     isTitleClicked
                       ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50"
                       : "translate-x-20 opacity-100"
-                  }`}
+                  } drop-shadow-4xl`}
                 >
                   WHEN YOU CROSS
                 </span>
@@ -70,7 +69,7 @@ const TitleCard = () => {
                     isTitleClicked
                       ? "-translate-x-20 opacity-100 "
                       : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50"
-                  }`}
+                  } drop-shadow-4xl`}
                 >
                   {titles.title2}
                 </span>
@@ -85,7 +84,7 @@ const TitleCard = () => {
                     isTitleClicked
                       ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50"
                       : "translate-x-20 opacity-100"
-                  }`}
+                  } drop-shadow-4xl`}
                 >
                   A VFX ARTIST AND DEVELOPER
                 </span>
@@ -94,7 +93,7 @@ const TitleCard = () => {
                     isTitleClicked
                       ? "-translate-x-20 opacity-100 "
                       : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50"
-                  }`}
+                  } drop-shadow-4xl`}
                 >
                   {titles.title3}
                 </span>
@@ -104,7 +103,6 @@ const TitleCard = () => {
         </div>
       </div>
 
-      {/* Optional: Overlay for styling (e.g., darkening the background) */}
       <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-40 z-5"></div>
     </div>
   );
