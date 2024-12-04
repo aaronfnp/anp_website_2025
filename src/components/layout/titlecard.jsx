52859f33cb662dcec575dfa93d47cad5e6f0039b
aaronfnp <aaronfnp@gmail.com>
2024-12-04 17:23:30 -0500

    responsive design
    - added max width so doesn't get too large
    - added opacity 0 for off screen content
    - added flex container so never gets stuck inbetween screen sizes

--- src/components/layout/titlecard.jsx
+++ src/components/layout/titlecard.jsx
@@ -24,84 +24,86 @@ const TitleCard = () => {
       </video>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col justify-center items-center h-full text-[#f6f9ff]">
-        <div
-          className="relative group text-center cursor-pointer"
-          onClick={() => setIsTitleClicked(true)}
-        >
-          <h1 className="flex flex-col text-[#f6f9ff] my-4 text-6xl md:text-8xl lg:text-9xl xl:text-[10rem] font-bebas font-extrabold drop-shadow-4xl ease-in-out transform transition-all whitespace-nowrap">
-            {/* Title Group 1 */}
-            <div className="inline-block">
-              <h2 className="flex justify-between w-full">
-                <span
-                  className={`duration-700 text-left ${
-                    isTitleClicked
-                      ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50"
-                      : "translate-x-8 md:translate-x-20 opacity-100"
-                  } drop-shadow-4xl`}
-                >
-                  WHAT DO YOU GET
-                </span>
-                <span
-                  className={`transition-all duration-700 ease-in-out transform text-right ${
-                    isTitleClicked
-                      ? "-translate-x-8 md:-translate-x-20 opacity-100 "
-                      : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50"
-                  } drop-shadow-4xl`}
-                >
-                  {titles.title1}
-                </span>
-              </h2>
-            </div>
+      <div className="relative z-10 flex justify-center items-center h-full text-[#f6f9ff]">
+        <div className="flex flex-col items-center max-w-screen-xl w-full px-4 sm:px-8">
+          <div
+            className="group text-center cursor-pointer"
+            onClick={() => setIsTitleClicked(true)}
+          >
+            <h1 className="flex flex-col text-[#f6f9ff] my-4 text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-bebas font-extrabold drop-shadow-4xl ease-in-out transform transition-all whitespace-nowrap">
+              {/* Title Group 1 */}
+              <div className="inline-block">
+                <h2 className="flex justify-between w-full">
+                  <span
+                    className={`duration-700 text-left ${
+                      isTitleClicked
+                        ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50 opacity-0"
+                        : "translate-x-8 md:translate-x-20 opacity-100"
+                    } drop-shadow-4xl`}
+                  >
+                    WHAT DO YOU GET
+                  </span>
+                  <span
+                    className={`transition-all duration-700 ease-in-out transform text-right ${
+                      isTitleClicked
+                        ? "-translate-x-8 md:-translate-x-20 opacity-100"
+                        : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50 opacity-0"
+                    } drop-shadow-4xl`}
+                  >
+                    {titles.title1}
+                  </span>
+                </h2>
+              </div>
 
-            {/* Title Group 2 */}
-            <div className="inline-block">
-              <h2 className="flex justify-between w-full">
-                <span
-                  className={`duration-700 text-left ${
-                    isTitleClicked
-                      ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50"
-                      : "translate-x-8 md:translate-x-20 opacity-100"
-                  } drop-shadow-4xl`}
-                >
-                  WHEN YOU CROSS
-                </span>
-                <span
-                  className={`transition-all duration-700 ease-in-out transform text-right ${
-                    isTitleClicked
-                      ? "-translate-x-8 md:-translate-x-20 opacity-100 "
-                      : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50"
-                  } drop-shadow-4xl`}
-                >
-                  {titles.title2}
-                </span>
-              </h2>
-            </div>
+              {/* Title Group 2 */}
+              <div className="inline-block">
+                <h2 className="flex justify-between w-full">
+                  <span
+                    className={`duration-700 text-left ${
+                      isTitleClicked
+                        ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50 opacity-0"
+                        : "translate-x-8 md:translate-x-20 opacity-100"
+                    } drop-shadow-4xl`}
+                  >
+                    WHEN YOU CROSS
+                  </span>
+                  <span
+                    className={`transition-all duration-700 ease-in-out transform text-right ${
+                      isTitleClicked
+                        ? "-translate-x-8 md:-translate-x-20 opacity-100"
+                        : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50 opacity-0"
+                    } drop-shadow-4xl`}
+                  >
+                    {titles.title2}
+                  </span>
+                </h2>
+              </div>
 
-            {/* Title Group 3 */}
-            <div className="inline-block">
-              <h2 className="flex justify-between w-full">
-                <span
-                  className={`duration-700 text-left ${
-                    isTitleClicked
-                      ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50"
-                      : "translate-x-8 md:translate-x-20 opacity-100"
-                  } drop-shadow-4xl`}
-                >
-                  A VFX ARTIST AND DEVELOPER
-                </span>
-                <span
-                  className={`transition-all duration-700 ease-in-out transform text-right ${
-                    isTitleClicked
-                      ? "-translate-x-8 md:-translate-x-20 opacity-100 "
-                      : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50"
-                  } drop-shadow-4xl`}
-                >
-                  {titles.title3}
-                </span>
-              </h2>
-            </div>
-          </h1>
+              {/* Title Group 3 */}
+              <div className="inline-block">
+                <h2 className="flex justify-between w-full">
+                  <span
+                    className={`duration-700 text-left ${
+                      isTitleClicked
+                        ? "-translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-white/50 to-black/50 opacity-0"
+                        : "translate-x-8 md:translate-x-20 opacity-100"
+                    } drop-shadow-4xl`}
+                  >
+                    A VFX ARTIST AND DEVELOPER
+                  </span>
+                  <span
+                    className={`transition-all duration-700 ease-in-out transform text-right ${
+                      isTitleClicked
+                        ? "-translate-x-8 md:-translate-x-20 opacity-100"
+                        : "translate-x-full opacity-100 text-transparent bg-clip-text bg-gradient-to-r from-black/50 to-white/50 opacity-0"
+                    } drop-shadow-4xl`}
+                  >
+                    {titles.title3}
+                  </span>
+                </h2>
+              </div>
+            </h1>
+          </div>
         </div>
       </div>
 
